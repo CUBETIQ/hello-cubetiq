dd193bfd35d545630bc255f9436fe634bf7b2977
Sambo Chea <7059827+sombochea@users.noreply.github.com>
2022-08-22 17:12:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ app.get('/', function (req, res) {
   res.send('Hello from CUBETIQ!')
 })
 
+app.get('/info', function (req, res) {
+  res.json({
+    status: 'OK',
+    message: 'Hello, CUBETIQ Solution',
+  })
+})
+
 app.listen(PORT, function () {
   console.log(`app listening on port ${PORT}!`)
 })
